6b578fdd471a6f518a9912d1333c2a8dc92032a1
Blake John <2991151588@qq.com>
2025-08-19 10:08:15 +0800

    new config of NvChad

---
+++ lua/options.lua
@@ -0,0 +1,12 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.guifont = "CaskaydiaMono Nerd Font:h12"
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+vim.g.lazyvim_python_lsp = "basedpyright"
